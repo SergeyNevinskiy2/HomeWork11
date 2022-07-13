00de19338d209ef01995a2e80ca84af3ea76cb16
SergeyNevinskiy <s.nevinskiy1987@ukr.net>
2022-07-13 22:16:35 +0300

    Ok

---
+++ script/script.js
@@ -0,0 +1,78 @@
+'use strict'
+// 1
+// const a = +prompt('Введите число');
+// a === 0 ? alert('Верно') : alert('Неверно');
+// 2
+// const a = +prompt('Введите число');
+// a > 0 ? alert('Верно') : alert('Неверно');
+// 3
+// const a = +prompt('Введите число');
+// a < 0 ? alert('Верно') : alert('Неверно');
+// 4
+// const a = +prompt('Введите число');
+// a >= 0 ? alert('Верно') : alert('Неверно');
+// 5
+// const a = +prompt('Введите число');
+// a <= 0 ? alert('Верно') : alert('Неверно');
+// 6
+// const a = +prompt('Введите число');
+// a !== 0 ? alert('Верно') : alert('Неверно');
+// 7
+// const a = prompt('Введите текст');
+// a === 'test' ? alert('Верно') : alert('Неверно');
+// 8
+// const a = prompt('Введите текст');
+// a === '1' ? alert('Верно') : alert('Неверно');
+// 9
+// const a = +prompt('Введите число');
+// a > 0 && a < 5  ? alert('Верно') : alert('Неверно');
+// 10
+// const a = +prompt('Введите число');
+// if (a === 0 || a === 2) {
+//     a += 7;
+// }else {
+//     a /= 10;
+// }
+// 11
+// const a = +prompt('Введите число');
+// const b = +prompt('Введите число');
+
+// if( a <= 1 || b >= 3 ) {
+//     const sum = a+b;
+//     alert(sum);
+// }else{
+//     const sub = a-b;
+//     alert(sub);
+// }
+// 12
+// const a = +prompt('Введите число');
+// const b = +prompt('Введите число');
+// if((a > 2 && a < 11) || (b >= 6 && b < 14)) {
+//     alert('Верно');
+// }else{
+//     alert ('Неверно')
+// };
+// 13
+// const num = +prompt('Введите число от 1 до 4');
+// let result = null;
+// switch(num) {
+//     case 1 :
+//         result = 'Зима'
+//         alert(result)
+//         break;
+//     case 2 :
+//         result = 'Весна'
+//         alert(result)
+//         break;
+//     case 3 :
+//         result = 'Лето'
+//         alert(result)
+//         break;
+//     case 4 :
+//         result = 'Осень'
+//         alert(result)
+//         break;
+//     default :
+//         alert('Ввели неврное значаение')   
+// };
+
